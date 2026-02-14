0865fa5c048a997fc0973cf1e29da96f78620d4c
Sai Smrithi Muthukumar <sai.muthukumar@bounteous.com>
2026-02-13 23:27:54 -0800

    Java code for best time to buy and sell stock 2 added

---
+++ arrays_and_strings/best_time_to_buy_and_sell_stock_2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max_profit = 0;
+        for(int i = 0; i < prices.length - 1; i++)
+        {
+            if(prices[i+1] - prices[i] >= 0)
+            {
+                max_profit += prices[i+1] - prices[i];
+            }
+        }
+
+        return max_profit;
+  
+    }
+}
