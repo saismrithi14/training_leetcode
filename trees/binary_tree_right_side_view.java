c26d14151cc2b28770828dee93e78897f946ff63
Sai Smrithi Muthukumar <sai.muthukumar@bounteous.com>
2026-02-19 08:57:07 +0530

    Java code to find right side of binary tree pushed

---
+++ trees/binary_tree_right_side_view.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        traversal(root, 0, map);
+        ArrayList<Integer> final_list = new ArrayList<>();
+        for(Map.Entry<Integer, ArrayList<Integer>> entry: map.entrySet())
+        {
+            int size = entry.getValue().size();
+            final_list.add(entry.getValue().get(size - 1));
+        }
+
+        return final_list;
+        
+    }
+
+    private void traversal(TreeNode root, int depth, HashMap<Integer, ArrayList<Integer>> map)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        else
+        {
+            if(map.containsKey(depth))
+            {
+                map.get(depth).add(root.val);
+            }
+
+            else
+            {
+                ArrayList<Integer> a1 = new ArrayList<>();
+                a1.add(root.val);
+                map.put(depth, a1);
+            }
+
+            traversal(root.left, depth+1,map);
+            traversal(root.right, depth+1,map);
+        }
+    }
+}
