12c68e9995fbef0bc403eb25a238260ab7494ffa
Sai Smrithi Muthukumar <sai.muthukumar@bounteous.com>
2026-02-23 11:47:05 +0530

    Code to find the depth of the binary tree added

---
+++ trees/depth_of_binary_tree.java
@@ -0,0 +1,12 @@
+
+public class depth_of_binary_tree {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+        {
+            return 0;
+        }
+
+        return Math.max(this.maxDepth(root.left), this.maxDepth(root.right)) + 1;
+
+    }
+}
