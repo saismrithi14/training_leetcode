20c1bc8753f4d8d0c8752a8ab34af8f49f70c73e
Sai Smrithi Muthukumar <sai.muthukumar@bounteous.com>
2026-02-23 11:39:13 +0530

    Code to find the level order traversal of the tree has been pushed

---
+++ trees/level_order_traversal.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ */
+import java.util.*;
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+public class level_order_traversal{
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
+        traversal(root, map, 0);
+        List<List<Integer>> final_list = new ArrayList<>();
+        for(Map.Entry<Integer, List<Integer>> entry: map.entrySet())
+        {
+            final_list.add(entry.getValue());
+        }
+
+        return final_list;
+    }
+
+    private void traversal(TreeNode root, HashMap<Integer, List<Integer>> map, int depth)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        if(!map.containsKey(depth))
+        {
+            ArrayList<Integer> a1 = new ArrayList<>();
+            a1.add(root.val);
+            map.put(depth, a1);
+        }
+
+        else
+        {
+            map.get(depth).add(root.val);
+        }
+
+        traversal(root.left, map, depth+1);
+        traversal(root.right, map, depth+1);
+    }
+}
