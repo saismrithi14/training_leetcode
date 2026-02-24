33da7e4e354dbc82e8f9d1b1ed351b45ed428608
Sai Smrithi Muthukumar <sai.muthukumar@bounteous.com>
2026-02-24 20:05:25 +0530

    Code to find H index added

---
+++ arrays_and_strings/H-index.java
@@ -0,0 +1,17 @@
+public class H-index {
+    public int hIndex(int[] citations) {
+        int h = 0;
+        Arrays.sort(citations);
+        int length = citations.length;
+        for(int i = length - 1; i >= 0; i--)
+        {
+            if(citations[i] >= length-i)
+            {
+                h = length-i;
+            }
+        }
+
+        return h;
+
+    }
+}
