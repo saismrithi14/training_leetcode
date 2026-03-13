ae38f734e03a9263ead32f66bbecd82db9463294
Sai Smrithi Muthukumar <sai.muthukumar@bounteous.com>
2026-03-13 11:43:37 +0530

    Java code to find out if there are any duplicates has been pushed

---
+++ hashmap/contains_duplicate_2.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+public class contains_duplicate_2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(!map.containsKey(nums[i]))
+            {
+                map.put(nums[i],i);
+            }
+
+            else
+            {
+                int difference = Math.abs(i-map.get(nums[i]));
+                if(difference <= k)
+                {
+                    return true;
+                }
+
+                map.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+}
