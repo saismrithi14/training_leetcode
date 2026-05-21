ccc35eff31c7dac999a89577232690fa3648e7da
Sai Smrithi Muthukumar <sai.muthukumar@bounteous.com>
2026-05-21 10:52:41 +0530

    Java code to find the minimum number of partitions to divide the number into deci binary numbers added

---
+++ math/MinimumPartitionsToSplitIntoDecyBinary.java
@@ -0,0 +1,16 @@
+public class MinimumPartitionsToSplitIntoDecyBinary {
+    public int minPartitions(String n) {
+        int maxDigit = 0;
+        for(int i = 0; i < n.length(); i++)
+        {
+            int digit = n.charAt(i) - '0';
+            if(digit > maxDigit)
+            {
+                maxDigit = digit;
+            }
+        }
+
+        return maxDigit;
+
+    }
+}
